d0c1f251c10a3f16c9dfc06836afe3b4667e78cf
Travis Meamber <travismeamber@Traviss-MacBook-Pro.local>
2017-11-06 09:03:14 -0700

    css

--- src/components/Shoecard.js
+++ src/components/Shoecard.js
@@ -4,7 +4,7 @@ import Shoecard from './Shoecard.css'
 export default function Brand(props) {
     console.log(props.brand)
     return (
-        <div>
+        <div className="shoecard">
             <img className="image" src={props.image} alt="Images" />
             <h2 className="shoebrand">{props.brand}</h2>
             <h1 className="name">{props.name}</h1>
