05e0f02624532770031fd062fd5c0bcb5d8d8a0b
Travis Meamber <travismeamber@Traviss-MacBook-Pro.local>
2017-11-09 14:58:17 -0700

    changed stuff

--- server/index.js
+++ server/index.js
@@ -62,7 +62,7 @@ passport.deserializeUser(function (id, done) {
 
 app.get('/auth', passport.authenticate('auth0'));
 app.get('/auth/callback', passport.authenticate('auth0', {
-    successRedirect: 'http://localhost:3000/',
+    successRedirect: process.env.SUCCESS_REDIRECT,
     failureRedirect: '/auth'
 }))
 app.get('/auth/me', (req, res) => {
